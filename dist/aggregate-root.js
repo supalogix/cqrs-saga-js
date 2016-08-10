0d79448ae4c4dca3ef1162ec740abb62e3b72460
Jonathan Nacionales <jnaciona@gmail.com>
2016-08-09 17:58:48 -0700

    Add generated files

--- dist/aggregate-root.js
+++ dist/aggregate-root.js
@@ -10,34 +10,115 @@ var _redux = require("redux");
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+var MARK_CHANGES_AS_COMMITTED = "@@cqrs-redux-js/events/MARK_CHANGES_AS_COMMITTED";
+var _markChangesAsCommitted = function _markChangesAsCommitted() {
+	return {
+		type: MARK_CHANGES_AS_COMMITTED
+	};
+};
+
+var entityChangesReducer = function entityChangesReducer() {
+	var state = arguments.length <= 0 || arguments[0] === undefined ? [] : arguments[0];
+	var event = arguments[1];
+
+	if (event.type === '@@redux/INIT') return [];
+
+	if (event.type === MARK_CHANGES_AS_COMMITTED) return [];
+
+	return state.concat(event);
+};
+
 var AggregateRoot = function () {
-	function AggregateRoot(reducer) {
+	/**
+  * @param {array} events
+  * @param {function} entityStateReducer
+  * @param {function} initialState
+  * @example <caption>Create Subclass</caption>
+  * const initialState = {};
+  * const reducer = (state, event) => {
+  *
+  * };
+  * class DomainEntity extends AggregateRoot {
+  *  constructor(events, props) {
+  *    super(events, reducer, initialState);
+  *  }
+  * }
+  */
+
+	function AggregateRoot(events, entityStateReducer, initialState) {
 		_classCallCheck(this, AggregateRoot);
 
-		this.store = (0, _redux.createStore)(reducer);
-		this.events = [];
+		var appState = events.reduce(function (state, event) {
+			return entityStateReducer(state, event);
+		}, initialState);
+
+		var reducers = {
+			entityState: entityStateReducer,
+			entityChanges: entityChangesReducer };
+		this.store = (0, _redux.createStore)((0, _redux.combineReducers)(reducers), appState);
 	}
+
+	/**
+  * @return {object}
+  */
+
 
 	_createClass(AggregateRoot, [{
 		key: "applyChange",
+
+
+		/**
+   * We need a means of recording changes that happen to an aggregate
+   * root.
+   *
+   * @param {object} event a domain event that you would like to save
+   *
+   * @example <caption>Add Domain Event to Entity Changes</caption>
+   * this.applyChange({
+   *   aggregateId: "",
+   *   version: 1,
+   *   type: ACCOUNT_CREATED
+   * })
+   */
 		value: function applyChange(event) {
 			this.store.dispatch(event);
-			this.events.push(event);
 		}
+
+		/**
+   * Clients need a means of retrieving the changes that have happened
+   * to an aggregate root.
+   *
+   * @return {array}
+   *
+   * @example <caption>Publish Domain Events</caption>
+   * publisher.publish(entity.getUncommittedChanges())
+   *
+   */
+
 	}, {
 		key: "getUncommittedChanges",
 		value: function getUncommittedChanges() {
-			return this.events;
+			return this.store.getState().entityChanges;
 		}
+
+		/**
+   * An aggregate root needs to let clients clear all its domain events
+   * once they have processed the domain events.
+   *
+   * @example <caption>Clear Domain Events After Publishing</caption>
+   * publisher.publish(entity.getUncommittedChanges());
+   * entity.markChangesAsCommitted();
+   */
+
 	}, {
 		key: "markChangesAsCommitted",
 		value: function markChangesAsCommitted() {
-			this.events = [];
+			this.store.dispatch(_markChangesAsCommitted());
 		}
 	}, {
 		key: "state",
 		get: function get() {
-			return this.store.getState();
+			return this.store.getState().entityState;
 		}
 	}]);
 
